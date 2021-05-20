ea9ded7d4ead8c201f6c3467909945a8bcf306e5
Sebastião Marcos <marcos.sms@gmail.com>
2021-05-19 21:53:29 -0300

    aula19-05-21

---
+++ Dart/index.dart
@@ -0,0 +1,30 @@
+//A classe main() é classe principal, obrigatória
+
+void main() {
+  //Imprimindo alguma coisa no Dart
+  print('Hello Dart');
+  print('Como vai?');
+
+  //Tipos de variáveis em Dart
+  //Dart faz inferência de tipo
+  /*var nome = 'John Doe';
+  var numero = 10;
+
+  //Se eu tentar colocar outro valor na variável nome
+  var nome = 'José';*/
+
+  String novoNome = 'Jane Doe'; //Estou tipando a minha variável
+  String CPF = '999.999.999-00';
+
+  //Definindo os inteiros
+  int ano = 1970;
+
+  //Definindo ponto flutuante
+  double altura = 1.77;
+
+  //Definição de booleanos
+  bool vf = true;
+  bool fv = false;
+
+  
+}
